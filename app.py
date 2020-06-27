77effea01c34bbce0048a9a3ff89da39ba5cee73
Nesim Pllana <pess-niam@outlook.com>
2020-06-27 20:44:28 +0200

    test

--- app.py
+++ app.py
@@ -12,64 +12,61 @@ LOG.setLevel(logging.INFO)
 
 
 def scale(payload):
-	"""Scales Payload"""
+		"""Scales Payload"""
 
-	LOG.info(f"Scaling Payload: \n{payload}")
-	scaler = StandardScaler().fit(payload.astype(float))
-	scaled_adhoc_predict = scaler.transform(payload.astype(float))
-	return scaled_adhoc_predict
+		LOG.info(f"Scaling Payload: \n{payload}")
+		scaler = StandardScaler().fit(payload.astype(float))
+		scaled_adhoc_predict = scaler.transform(payload.astype(float))
+		return scaled_adhoc_predict
 
 
 @app.route("/")
 def home():
-	html = f"<h3>Sklearn Prediction Home</h3>"
-	return html.format(format)
+	html = f"<h3>Sklearn Prediction Home</h3>".format(format)
+	return html
 
 
 @app.route("/predict", methods=['POST'])
 def predict():
 	"""Performs an sklearn prediction
 
-			input looks like:
-			{
-			"CHAS":{
-			"0":0
-			},
-			"RM":{
-			"0":6.575
-			},
-			"TAX":{
-			"0":296.0
-			},
-			"PTRATIO":{
-			"0":15.3
-			},
-			"B":{
-			"0":396.9
-			},
-			"LSTAT":{
-			"0":4.98
-			}
+        input looks like:
+        {
+        "CHAS":{
+        "0":0
+        },
+        "RM":{
+        "0":6.575
+        },
+        "TAX":{
+        "0":296.0
+        },
+        "PTRATIO":{
+        "0":15.3
+        },
+        "B":{
+        "0":396.9
+        },
+        "LSTAT":{
+        "0":4.98
+        }
 
-			result looks like:
-			{ "prediction": [ <val> ] }
+        result looks like:
+        { "prediction": [ <val> ] }
 
-			"""
+        """
 
 	# Logging the input payload
 	json_payload = request.json
 	LOG.info(f"JSON payload: \n{json_payload}")
 	inference_payload = pd.DataFrame(json_payload)
 	LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
-
 	# scale the input
 	scaled_payload = scale(inference_payload)
-
 	# get an output prediction from the pretrained model, clf
 	prediction = list(clf.predict(scaled_payload))
-
-	# Log the output prediction value
-	LOG.info(f"Output prediction: {prediction}")
+	# TO DO:  Log the output prediction value
+	LOG.info(f"Prediction Log: \n{prediction}")
 	return jsonify({'prediction': prediction})
 
 
