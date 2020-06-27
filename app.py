07766d2039069a7e83af6aa1e8aeb1ff96e67c60
Nesim Pllana <pess-niam@outlook.com>
2020-06-27 20:42:24 +0200

    test

--- app.py
+++ app.py
@@ -22,51 +22,54 @@ def scale(payload):
 
 @app.route("/")
 def home():
-	html = f"<h3>Sklearn Prediction Home</h3>".format(format)
-	return html
+	html = f"<h3>Sklearn Prediction Home</h3>"
+	return html.format(format)
 
 
 @app.route("/predict", methods=['POST'])
 def predict():
 	"""Performs an sklearn prediction
 
-        input looks like:
-        {
-        "CHAS":{
-        "0":0
-        },
-        "RM":{
-        "0":6.575
-        },
-        "TAX":{
-        "0":296.0
-        },
-        "PTRATIO":{
-        "0":15.3
-        },
-        "B":{
-        "0":396.9
-        },
-        "LSTAT":{
-        "0":4.98
-        }
+			input looks like:
+			{
+			"CHAS":{
+			"0":0
+			},
+			"RM":{
+			"0":6.575
+			},
+			"TAX":{
+			"0":296.0
+			},
+			"PTRATIO":{
+			"0":15.3
+			},
+			"B":{
+			"0":396.9
+			},
+			"LSTAT":{
+			"0":4.98
+			}
 
-        result looks like:
-        { "prediction": [ <val> ] }
+			result looks like:
+			{ "prediction": [ <val> ] }
 
-        """
+			"""
 
 	# Logging the input payload
 	json_payload = request.json
 	LOG.info(f"JSON payload: \n{json_payload}")
 	inference_payload = pd.DataFrame(json_payload)
 	LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+
 	# scale the input
 	scaled_payload = scale(inference_payload)
+
 	# get an output prediction from the pretrained model, clf
 	prediction = list(clf.predict(scaled_payload))
-	# TO DO:  Log the output prediction value
-	LOG.info(f"Prediction Log: \n{prediction}")
+
+	# Log the output prediction value
+	LOG.info(f"Output prediction: {prediction}")
 	return jsonify({'prediction': prediction})
 
 
